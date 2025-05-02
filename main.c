bd6606cb44dbe1ddd7db8c5e64a1dfdd8b75a3bf
Tsevetok <gabrielmoita34@gmail.com>
2025-05-02 15:41:41 +0100

    Made files work for the a subrask of task 1 Finally some good progress

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include<string.h>
 #include<stdbool.h>
 #define DIM 10
+//because C doesn't have THE SIMPLE FUNCTION OF A TRY CATCH we have to do it myself
+#define try bool __HadError=false;
+#define catch(x) ExitJmp:if(__HadError)
+#define throw(x) {__HadError=true;goto ExitJmp;}
+
 
 struct tarefa{
     char name[24];
@@ -20,14 +25,17 @@ int resposibles_number = 1;
 char responsible[24] = "";
 //Registar uma nova tarefa
 void register_new_task(tarefa *task){
-    tarefas = fopen("C:\\Users\\brash\\OneDrive\\Ambiente de Trabalho\\oihn\\tarefas.csv", "r");
+    tarefas = fopen("C:\\Users\\brash\\OneDrive\\Ambiente de Trabalho\\oihn\\tarefas.csv", "a");
+
+    //Start new line in file
+    fputs("\n", tarefas);
 
     //TODO: substitute strcpy to insert data into the file
 
     fseek(tarefas, 0,EOF);
 
     //name
-    char responsibles[10][10];
+
     printf("\nPlease give the task a name\n");
     //scanf("%s", task.name);
     strcpy(task->name, "test name");
@@ -38,25 +46,43 @@ void register_new_task(tarefa *task){
     printf("\nplease list how many resposibles there are\n"); //TODO: change this for file format for teams and stuff
     //scanf("%d", &resposibles_number);
     resposibles_number = 1;
-
+    if(resposibles_number != 1){
+        fputs("Equipa 1: ", tarefas);
+    }
     for(int i = 0; i < resposibles_number; i++){
         printf("\nplease write the name of the responsible number %d\n", i);
         //scanf("%s", responsible);
         strcpy(responsible, "some beutiful name");
-        strcpy(task->responsibles[i], responsible);
+        fputs(responsible, tarefas);
+        fputs(", ", tarefas);
+        //strcpy(task->responsibles[i], responsible);
     }
+
+    fputs(";", tarefas);
 
     printf("\nplease inserts today's date in the format yyyymmdd\n");
     //scanf("%f", &task->creation_date);
     task->creation_date = 20250430;
+    char buffer[16] = {0};
+    sprintf(buffer, "%f", task->creation_date);
+    fputs(buffer, tarefas);
+
+    fputs(";", tarefas);
 
     printf("\nplease insert the limit date in the format yyyymmdd\n");
     task->lim_date = 20250431;
+    sprintf(buffer, "%f", task->lim_date);
+    fputs(buffer, tarefas);
+
+    fputs(";", tarefas);
+    
 
     printf("\nPlease write a small description of the task\n");
     //scanf("%s", &task->description);
     strcpy(task->description, "descirption");
+    fputs(task->description, tarefas);
 
+    fputs(";", tarefas);
     fclose(tarefas);
 }
 
@@ -106,7 +132,7 @@ int main(){
     struct tarefa current_task;
     while(1){
         printf("what action do you pretend to execute");
-        printf("Tarefas\n\t1- Registar nova tarfa\n\t2-alterar dados de uma tarefa\n\t3-Definir pessoa\n\t4-concluir tarefa\n\t5-eliminar uma tarefa");
+        printf("Tarefas\n\t1- Registar nova tarfa\n\t2-alterar dados de uma tarefa\n\t3-Definir pessoa\n\t4-concluir tarefa\n\t5-eliminar uma tarefa\n\t 6-stop process");
         scanf("%d", &choice);
 
         switch(choice){
@@ -115,6 +141,9 @@ int main(){
                 break;
             case(2): //Alterate task's data
                 change_data(&current_task);
+                break;
+            case(6):
+                return 1;
                 break;
         }
 
