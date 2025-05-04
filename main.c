f43d1345fc62dedda932cef1381d3880fc2038f4
Flower <gabrielmoita34@gmail.com>
2025-05-04 15:36:32 +0100

    changed the way files are handeled\ started lists thingyes

--- main.c
+++ main.c
@@ -3,10 +3,6 @@
 #include<string.h>
 #include<stdbool.h>
 #define DIM 10
-//because C doesn't have THE SIMPLE FUNCTION OF A TRY CATCH we have to do it myself
-#define try bool __HadError=false;
-#define catch(x) ExitJmp:if(__HadError)
-#define throw(x) {__HadError=true;goto ExitJmp;}
 
 
 struct tarefa{
@@ -16,28 +12,72 @@ struct tarefa{
     float lim_date;
     float conclusion_date;
     char description[30];
+
+    int responsible; //idicates if it's a team or not
+    char team[24];
 };
 typedef struct tarefa tarefa;
 
 FILE *tarefas;
 
+void write_file(tarefa *task){
+    tarefas = fopen("C:\\Users\\brash\\OneDrive\\Ambiente de Trabalho\\oihn\\tarefas.csv", "a");
+
+    char buffer[16] = {0};
+
+    //initiate new line
+    fputs("\n", tarefas);
+
+    //write task name
+    fputs(task->name, tarefas);
+    fputs(";", tarefas); //end section
+
+    //write responsibles
+    if(task->responsibles != *1){  //in case of multiple responsibles
+        char team = strcat("Equipa", task->team);
+        
+        fputs(team, tarefas);
+    }
+    for(int i = 0; i < resposibles_number; i++){
+        fputs(task->responsibles[i], tarefas);
+        fputs(", ", tarefas);
+    }
+    fputs(";", tarefas);
+
+    //write creation date
+    sprintf(buffer, "%0.0f", task->creation_date);
+    fputs(buffer, tarefas);
+    fputs(";", tarefas);
+
+    //write limit date
+    spintf(buffer, "%0.0f", task->lim_date);
+    fputs(buffer, tarefas);
+    fputs(";", tarefas);
+
+    if(task->conclusion_date != 0){
+        spintf(buffer, "%0.0f", task->conclusion_date);
+        fputs(buffer, tarefas);
+    } else {
+        fputs("on-going", tarefas);
+    }
+    fputs(";", tarefas);
+
+    //write description
+    fputs(task->description, tarefas);
+    fputs(";", tarefas);
+    fclose(tarefas);
+}
+
+
 int resposibles_number = 1;
 char responsible[24] = "";
 //Registar uma nova tarefa
 void register_new_task(tarefa *task){
-
-    //TODO: substitute strcpy to insert data into the file
-
-    //fseek(tarefas, 0,EOF);
-
     //name
 
     printf("\nPlease give the task a name\n");
     //scanf("%s", task.name);
     strcpy(task->name, "test name");
-    fputs(task->name, tarefas);
-
-    fputs(";", tarefas);
 
     printf("\nplease list how many resposibles there are\n"); //TODO: change this for file format for teams and stuff
     //scanf("%d", &resposibles_number);
@@ -47,39 +87,27 @@ void register_new_task(tarefa *task){
     }
     for(int i = 0; i < resposibles_number; i++){
         printf("\nplease write the name of the responsible number %d\n", i);
-        //scanf("%s", responsible);
+         //scanf("%s", responsible);
         strcpy(responsible, "some beutiful name");
-        fputs(responsible, tarefas);
-        fputs(", ", tarefas);
+        
         //strcpy(task->responsibles[i], responsible);
     }
 
-    fputs(";", tarefas);
 
     printf("\nplease inserts today's date in the format yyyymmdd\n");
     //scanf("%f", &task->creation_date);
     task->creation_date = 20250430;
-    char buffer[16] = {0};
-    sprintf(buffer, "%f", task->creation_date);
-    fputs(buffer, tarefas);
-
-    fputs(";", tarefas);
 
     printf("\nplease insert the limit date in the format yyyymmdd\n");
+    //scanf("%f", &task->lim_date);
     task->lim_date = 20250431;
-    sprintf(buffer, "%f", task->lim_date);
-    fputs(buffer, tarefas);
-
-    fputs(";", tarefas);
     
+    task->lim_date = 0;
 
     printf("\nPlease write a small description of the task\n");
     //scanf("%s", &task->description);
     strcpy(task->description, "descirption");
-    fputs(task->description, tarefas);
-
-    fputs(";", tarefas);
-    fclose(tarefas);
+    
 }
 
 void change_data(tarefa *task){ //modify "data limite de execusao" "O responsavel" ou a "descicao"
@@ -126,6 +154,16 @@ void change_data(tarefa *task){ //modify "data limite de execusao" "O responsave
     }
 }
 
+void change_person(tarefa *task){ //TODO: this
+    printf("please");
+}
+
+void set_complete(tarefa *task){
+    printf("please insert what date it is");
+    scanf("%f", task->conclusion_date);
+
+}
+
 void eliminate_task(int delete_line){
     FILE *fileptr1, *fileptr2;
     char filename[40] = "tarefas.csv";
@@ -133,7 +171,7 @@ void eliminate_task(int delete_line){
     int temp = 1;
 
     //open file in read mode
-    fileptr1 = fopen("C:\\Users\\brash\\OneDrive\\Ambiente de Trabalho\\oihn\\tarefas.csv", "r");
+    fileptr1 = fopen("tarefas.csv", "r");
     ch = getc(fileptr1);
    while (ch != EOF){
         printf("%c", ch);
@@ -160,7 +198,6 @@ void eliminate_task(int delete_line){
     remove(filename);
     //rename the file replica.c to original name
     rename("replica.c", filename);
-    printf("\n The contents of file after being modified are as follows:\n");
     fileptr1 = fopen(filename, "r");
     ch = getc(fileptr1);
     while (ch != EOF){
@@ -174,7 +211,7 @@ void eliminate_task(int delete_line){
 void criar_equipas(){
     FILE *file;
 
-    file = fopen("C:\\Users\\brash\\OneDrive\\Ambiente de Trabalho\\oihn\\output\\teams.txt", "a");
+    file = fopen("output/teams.txt", "a");
     fputs("\n", file);
     fputs("Equipa", file);
     char ch;
@@ -200,6 +237,45 @@ void criar_equipas(){
         fputs(name, file);
     }
 }
+void alocar_equipa(){
+    FILE *teams;
+
+    teams = fopen("output/teams.txt", "r");
+    char ch;
+    int choise, lines; 
+    bool flag = false;
+
+    printf("\nQue equipa pertende alocar para a tarefa?\n");
+    while (ch != EOF){
+        printf("%c", ch);
+        ch = getc(teams);
+    }
+    while(!feof(teams)){
+        ch = fgetc(teams);
+        if(ch == '\n'){
+            lines++;
+        }
+    }
+    scanf("%d", &choise);
+    
+
+    fclose(teams);
+}
+
+
+//LISTS
+void list_execution(tarefa *task){
+    FILE *file;
+
+    file = fopen("tarefas.csv", "r");
+    char line[265];
+
+    while(fgets(line, sizeof(line), file)){
+        if(strchr(line, 'on-going') != NULL){
+            printf("%s", line);
+        }
+    }
+}
 
 int choice;
 int main(){
@@ -216,6 +292,12 @@ int main(){
             case(2): //Alterate task's data
                 change_data(&current_task);
                 break;
+            case(3):
+                change_person(&current_task);
+                break;
+            case(4):
+                set_complete(&current_task);
+                break;
             case(5):
                 printf("\nwhat tarefa do you want to delete?\n");
                 int line;
@@ -225,6 +307,9 @@ int main(){
                 break;
             case(6):
                 criar_equipas();
+                break;
+            case(7):
+                alocar_equipa();
                 break;
             case(8):
                 return 1;
